0fa8074c3503e919c8074a551f51c54895ab7b43
DELL <lavanyamaha294@gmail.com>
2024-09-13 13:41:47 +0530

    changes

--- lib/Map/manuallocationscreen.dart
+++ lib/Map/manuallocationscreen.dart
@@ -69,7 +69,7 @@ class _ManualLocationScreenState extends State<ManualLocationScreen> {
               _buildLabel('Name'),
               _buildTextField(_nameController, 'Enter your Name'),
               SizedBox(height: 20),
-              _buildLabel('Door no / Flat no'),
+              _buildLabel('Door number / Flat no'),
               _buildTextField(_doorController, 'Enter your Door No'),
               SizedBox(height: 20),
               _buildLabel('Address'),
